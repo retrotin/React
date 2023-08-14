bb51adcfbb8469e6c60f992a020c1bece58692cb
retrotin <maartin.lopez@gmail.com>
2023-08-14 20:42:40 -0300

    2da entrega

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,35 @@
+import './ItemCount,css'
+import {useState} from 'react'
+
+const ItemCount =({stock, initial, onAdd})=> {
+    const [quantity, setQuantity] = useState(initial)
+
+    const increment = () => {
+        if (quantity < stock){
+            setQuantity(quantity+1)
+        }
+    }
+    
+    const decrement =() => {
+        if (quantity > 1) {
+            setQuantity (quantity-1)
+        }
+    }
+
+
+    return(
+        <div className = 'Counter'>
+            <div className = 'Constrols'>
+                <button className ='Button' onClick ={decrement}>-</button>
+                <h4 className= 'Number'> {quantity}</h4>
+                <button className= 'Button' onClick = {increment}>+</button>
+            </div>
+            <div>
+                <button className = "Button" onClick = {() => onAdd (quantity) } disable ={!stock}>
+                    Agregar al carrito
+                </button>
+            </div>
+            </div>    
+    
+    )
+}
